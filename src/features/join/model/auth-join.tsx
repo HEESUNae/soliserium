9a7519042b6c537121ee49b12d601e0076cd5ba9
HEESUNae <heesun9456@gmail.com>
2024-12-17 20:10:27 +0900

    🐞 fix: 회원가입 이름 정규식 변경

--- src/features/join/model/auth-join.tsx
+++ src/features/join/model/auth-join.tsx
@@ -16,7 +16,7 @@ export const checkRegex = (name: string, value: string) => {
   if (name === 'id') {
     isValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value); // 이메일
   } else if (name === 'name') {
-    isValid = /^[a-zA-Z가-힣]+$/.test(value); // 영문 + 숫자 포함
+    isValid = /^[a-zA-Z가-힣\s]+$/.test(value); // 영문 + 숫자 포함
   } else if (name === 'pw') {
     isValid = /^[A-Za-z0-9]{6,}$/.test(value); // 6자 이상, 영문 + 숫자 포함
   } else {
